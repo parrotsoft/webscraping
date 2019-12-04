749c03636a95f976003517e9f405d8cce1eef64a
Miguel Lopez <lopezarizamiguel@gmail.com>
2019-12-04 15:58:01 -0500

    Modificando el json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const url = 'https://www.olx.com.co/barranquilla_g4069245';
+const url = 'https://www.olx.com.co/items';
 request(url, (err, res, body) => {
     if (!err && res.statusCode == 200) {
         const $ = cheerio.load(body);
@@ -10,7 +10,9 @@ request(url, (err, res, body) => {
             return {
                 producto: $prod.find('._2tW1I').text(),
                 precio: $prod.find('._89yzn').text(),
-                link: `https://www.olx.com.co${$prod.find('a').attr('href')}`
+                link: `https://www.olx.com.co${$prod.find('a').attr('href')}`,
+                lugar: $prod.find('.tjgMj').text(),
+                fecha: $prod.find('.zLvFQ').text()
             }
         })
         console.log(list_prod);
